b781e99bfe4302d80b830ac9bcadfd2f2929e619
aibariok67-dotcom <aibariok67@gmail.com>
2026-01-16 18:52:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+const http = require('http');
 const WebSocket = require('ws');
 
-const PORT = process.env.PORT || 8080;
-const wss = new WebSocket.Server({ port: PORT });
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer();
+const wss = new WebSocket.Server({ server });
 
 wss.on('connection', ws => {
   ws.on('message', msg => {
     wss.clients.forEach(client => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(msg);
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg.toString());
       }
     });
   });
 });
 
-console.log('Server running on port', PORT);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log('Server running on port', PORT);
+});
+
